e9e2ca999e5da07e87616e4d848b4e89c5d9c5e3
Aji Bagaskara <ajibagaskara08@gmail.com>
2024-04-21 19:24:44 +0800

    Adding Changes

--- app/page.js
+++ app/page.js
@@ -53,7 +53,8 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+            Find in-depth information about Next.js features and API. HELLO
+            WORLD
           </p>
         </a>
 
@@ -71,6 +72,7 @@ export default function Home() {
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
+            CHANGES HERE
           </p>
         </a>
 
